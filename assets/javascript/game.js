45528d6a123d3aabae6dddaae7d5d58886af0565
thmiv <thmiv4@gmail.com>
2018-07-27 00:32:28 -0500

    game reset and word-selector working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,26 @@
 // declaring variables
-var word;   // The word to be played
+var word;   // current word being played
+var words;   // The words which can be played
 var guesses;    // number of available guesses
 var correctGuesses;    // letters guessed correctly
 var wrongGuesses;   // letters guessed incorrectly
+var gameWins = 0;   // number of times user won
+var gameLosses = 0;   // number of times user lost      
 
 // get HTML elements
 var currentWordEl = document.getElementById('currentWord');   // display current work element
 var chancesRemainingEl = document.getElementById('lettersRemaining');   // display remaining available guessed
 var lettersGuessedEl = document.getElementById('lettersGuessed');   //display the letters incorectly guessed
+var numWinsEl = document.getElementById('numWins');     // display number of times user won
+var numLossesEL = document.getElementById('numLoss');     // display number of times user lost
+
+// array of words 
+words = ["banzai", "coconut", "javascript"];
 
 // game initialization
 function setupGame() {
-    word = 'banzai';
-    guesses = 13;
+    word = words[Math.floor(Math.random() * words.length)];
+    guesses = word.length;
     wrongGuesses = [];
     correctGuesses = [];
 
@@ -20,12 +28,14 @@ function setupGame() {
     console.log(word);
 
     // creates underscores for concealed word
-    for (var i = 0; i < word.length; i++) {
+    for (i = 0; i < word.length; i++) {
         correctGuesses.push('_');
     }
 
     currentWordEl.innerHTML = correctGuesses.join(' ');
     chancesRemainingEl.innerHTML = guesses;
+    numWinsEl.innerHTML = gameWins;
+    numLossesEL.innerHTML = gameLosses;
 }
 
 // updates word with guessed letters
@@ -35,12 +45,12 @@ function guessUpdate(letter) {
 
     if (word.indexOf(letter) === -1) { // letter is NOT in the word
         wrongGuesses.push(letter); // update letters guessed
-        lettersGuessedEl.innerHTML = wrongGuesses.join(', ');
+        lettersGuessedEl.innerHTML = wrongGuesses.join(' - ').toUpperCase(); // append wrong letter to HTML
     } else { // letter IS in the word
         // replace underscore with the letter
-        for (var i = 0; i < word.length; i++) {
-            if (word[i] === letter) {
-                correctGuesses[i] = letter;
+        for (j = 0; j < word.length; j++) {
+            if (word[j] === letter) {
+                correctGuesses[j] = letter;
             }
         }
         currentWordEl.innerHTML = correctGuesses.join(' ');
@@ -52,15 +62,18 @@ function checkForWin() {
     var gameOver = false;
     if (correctGuesses.indexOf('_') === -1) {
         alert('You Won!');
+        gameWins++;
         gameOver = true;
     } else if (guesses === 0) {
         alert('You Lost!');
         gameOver = true;
+        gameLosses++;
     }
     if (gameOver === true) {
-        var gameStatus = confirm("Game is over. Would you like to refresh and play again?")
+        var gameStatus = confirm("Game is over. Would you like to play again?")
         if (gameStatus === true) {
-            location.reload();
+            lettersGuessedEl.innerHTML = " ";
+            setupGame();
         }
     }
 }
